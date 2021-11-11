8e2060577ebb0a001d6591ce81cf54f038e6871f
Mayank Singh <smayank748@gmail.com>
2021-11-11 09:09:02 +0530

    [Mayank] implementated the calculation of employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,20 @@
 #! /bin/bash -x
+
 echo "Welcom to emp wage computation "
 isPresent=1;
 checkAttandance=$(( $RANDOM % 10))
+chargeForOneHour=20
+noOfWorkingHour=0
+totalWage=0
 
 #To check the attandance
 if [ $isPresent -eq $checkAttandance ]
 then
-	echo "Emp is present "
+	echo "Emp is present"
+	noOfWorkingHour=8
 else
 	echo "Emp is not present "
+	noOfWorkingHour=0
 fi
+totalWage=$(( noOfWorkingHour*chargeForOneHour ))
+echo "Total Wage of emp "$totalWage
