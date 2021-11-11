67e6f7e49effbe61d8ba7aca84734a09299ded44
Mayank Singh <smayank748@gmail.com>
2021-11-11 08:48:55 +0530

    [Mayank] implementation of check attandance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,10 @@
+#! /bin/bash -x
 echo "Welcom to emp wage computation "
+isPresent=1;
+checkAttandance=$(( $RANDOM % 10))
+if [ $isPresent -eq $checkAttandance ]
+then
+	echo "Emp is present "
+else
+	echo "Emp is not present "
+fi
