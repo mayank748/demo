0038d95d56780ed7eb8bbbbb9297b6be50c472fd
Mayank Singh <smayank748@gmail.com>
2021-11-11 09:18:56 +0530

    [Mayank] implementing the calculation of part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,15 +6,21 @@ checkAttandance=$(( $RANDOM % 10))
 chargeForOneHour=20
 noOfWorkingHour=0
 totalWage=0
+isPartTime=2 #To check user is doing partime
 
 #To check the attandance
 if [ $isPresent -eq $checkAttandance ]
 then
 	echo "Emp is present"
 	noOfWorkingHour=8
+elif [ $isPartTime -eq $checkAttandance ]
+then
+	echo "Emp is doing partime"
+	noOfWorkingHour=4
 else
 	echo "Emp is not present "
 	noOfWorkingHour=0
 fi
+
 totalWage=$(( noOfWorkingHour*chargeForOneHour ))
 echo "Total Wage of emp "$totalWage
