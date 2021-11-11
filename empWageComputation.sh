27cd30140d038876836c23e98ec2a0880a1bda4f
Mayank Singh <smayank748@gmail.com>
2021-11-11 09:34:51 +0530

    [Mayank] we have removed the nested if wiht case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,19 +10,18 @@ noOfWorkingHour=0 #No fo working hour of one emp
 totalWage=0 #To store the total wage of the emp
 
 #To check the attandance
-if [ $isPresent -eq $checkAttandance ]
-then
-	echo "Emp is present"
-	noOfWorkingHour=8
-elif [ $isPartTime -eq $checkAttandance ]
-then
-	echo "Emp is doing partime"
-	noOfWorkingHour=4
-else
-	echo "Emp is not present "
-	noOfWorkingHour=0
-fi
-
+case $checkAttandance in
+	$isPresent)
+		echo "Emp is present"
+		noOfWorkingHour=8
+		;;
+	$isPartTime)
+		echo "Emp is doing parttime"
+		noOfWorkingHour=4
+		;;
+	*)
+		echo "Emp is adsent"
+esac
 #Calculating the wage of the emp
 totalWage=$(( noOfWorkingHour*chargeForOneHour ))
 echo "Total Wage of emp "$totalWage
