f134e781b8c176919c9a58dd83ff50ed0dd72675
Mayank Singh <smayank748@gmail.com>
2021-11-11 09:51:24 +0530

    [Mayank] implemted wage calculation  for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,11 +4,15 @@ echo "Welcom to emp wage computation "
 
 isPartTime=2 #To check user is doing partime
 isPresent=1; #To check if the emp is present
-checkAttandance=$(( $RANDOM % 10)) #To get for the check of emp attandance
 chargeForOneHour=20 #Charge per hour of the emp
 noOfWorkingHour=0 #No fo working hour of one emp
 totalWage=0 #To store the total wage of the emp
+totalWorkingDay=20 #To store to working days of emp
+totalWageInMonth=0 #To store the total wage of emp in month
 
+for (( counter=0;counter<totalWorkingDay;counter++ ))
+do
+checkAttandance=$(( $RANDOM % 10)) #To get for the check of emp attandance
 #To check the attandance
 case $checkAttandance in
 	$isPresent)
@@ -22,6 +26,13 @@ case $checkAttandance in
 	*)
 		echo "Emp is adsent"
 esac
+
 #Calculating the wage of the emp
-totalWage=$(( noOfWorkingHour*chargeForOneHour ))
-echo "Total Wage of emp "$totalWage
+totalWage=$(($totalWage+$(( noOfWorkingHour*chargeForOneHour ))))
+echo "Total Wage of emp in a day "$totalWage
+
+done
+
+#Calculating the total wage of emp in month
+totalWageInMonth=$totalWage
+echo "Total wage of emp in month " $totalWageInMonth
